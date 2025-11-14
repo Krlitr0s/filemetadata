c77a23ec4aee1d92c31ddfacdd5dbd39625cdcb3
Carlos Medina <carlos.medina@ipicyt.edu.mx>
2025-11-14 10:27:54 -0600

    my solution

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var express = require('express');
+const multer = require('multer');
+const path = require('path');
+
 var cors = require('cors');
 require('dotenv').config()
 
 var app = express();
-
+app.use(express.static('public'));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
 
+  const { originalname, mimetype, size } = req.file;
+
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
